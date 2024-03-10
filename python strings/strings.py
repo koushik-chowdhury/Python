324ab297ece7feb77952d03b859a8594d4cd49c8
Koushik Chowdhury <csakoushik@gmail.com>
2024-03-10 23:50:06 +0530

    ADD README FILE

---
+++ python strings/strings.py
@@ -0,0 +1,17 @@
+name = "Koushik"
+first_char = name[0] 
+print(first_char)
+
+full_name = "Koushik Chowdhury"
+fname = full_name[:8]
+print(fname)
+
+num_list = "0123456789"
+num_list[:]
+num_list[:10]
+num_list[1:]
+num_list[2:10:2]
+
+name
+print(name.lower())
+print(name.upper())
